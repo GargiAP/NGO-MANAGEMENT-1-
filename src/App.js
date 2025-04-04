4826795c1ddad68ef85c0c60a81328c482589006
GargiAP <gargipratapwar@gmail.com>
2025-04-04 21:54:57 +0530

    signin and login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-//import HomePage from './pages/homepage/Homepage';
-//import LoginPage from './pages/loginpage/Loginpage'
-import SigninPage from './pages/signinpage/Signinpage'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/Homepage";
+import LoginPage from "./pages/loginpage/Loginpage";
+import SigninPage from "./pages/signinpage/Signinpage";
+
 function App() {
   return (
-    <div className="App">
-      <SigninPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
+    </Router>
   );
 }
 
