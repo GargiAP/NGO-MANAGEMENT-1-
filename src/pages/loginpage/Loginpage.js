8c4ccf560b2525be985ea8a2b9092caa99e00f59
gargipratapwar@gmail.com <gargipratapwar@gmail.com>
2025-04-05 18:17:09 +0530

    pop rough design needs some fixes

--- src/pages/loginpage/Loginpage.js
+++ src/pages/loginpage/Loginpage.js
@@ -5,28 +5,29 @@ import './loginpage.css';
 const LoginPage = () => {
   return (
     <div className="login-wrapper">
-  <form className="login-card">
+      <form className="login-card">
+        <h1 className="login-title">Login</h1>
 
-    <h1 className="login-title">Login</h1>
-    
-    <div className="input-container">
-      <FaUser className="icon" />
-      <input type="text" placeholder="Username" required />
-    </div>
+        {}
+        <div className="flex-input-group">
+          <div className="input-container">
+            <FaUser className="icon" />
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div className="input-container">
+            <FaLock className="icon" />
+            <input type="password" placeholder="Password" required />
+          </div>
+        </div>
 
-    <div className="input-container">
-      <FaLock className="icon" />
-      <input type="password" placeholder="Password" required />
-    </div>
-                                                               
-    <button className="login-button" type="submit">Login</button>
+        <button className="login-button" type="submit">Login</button>
 
-    <div className="login-subtext">
-      <p>Don’t have an account?</p>
-      <p className="create-account">Create Account</p>
+        <div className="login-subtext">
+          <p>Don’t have an account?</p>
+          <p className="create-account">Create Account</p>
+        </div>
+      </form>
     </div>
-  </form>
-</div>
   );
 };
 
