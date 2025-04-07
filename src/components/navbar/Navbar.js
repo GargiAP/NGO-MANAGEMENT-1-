1eb4e3c0dfc32a8302e3ae6b497561ea542ecbb7
anchal munot <anchalmunot@anchals-MacBook-Air.local>
2025-04-07 09:41:50 +0530

    volunteer form

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom'; 
 import logo from '../../logo.svg';  
 
 const Navbar = ({ onLoginClick, onSigninClick }) => {
@@ -11,15 +12,13 @@ const Navbar = ({ onLoginClick, onSigninClick }) => {
 
       <ul className="nav-links">
         <li><a href="/">Home</a></li>
-        <li><a href="/volunteer">Volunteer</a></li>
+        <li><Link to="/volunteer">Volunteer</Link></li>
         <li><a href="/donation">Donation</a></li>
       </ul>
 
       <div className="button-container">
       <button className="button login-btn" onClick={onLoginClick}>Login</button>
-<button className="button signup-btn" onClick={onSigninClick}>Sign In</button>
-
-
+      <button className="button signup-btn" onClick={onSigninClick}>Sign Up</button>
       </div>
     </nav>
   );
