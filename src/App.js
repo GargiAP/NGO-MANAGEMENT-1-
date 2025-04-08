3e7a653f4a51ab2220a7266903e7253178f3858c
anchal munot <anchalmunot@anchals-MacBook-Air.local>
2025-04-08 16:47:11 +0530

    signin both final with close button & volunteer solved

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import VolunteerPage from "./pages/volunteerpg/Volunteerpg";
 import LoginPage from "./pages/loginpage/Loginpage";
 import SigninPage from "./pages/signinpage/Signinpage";
 import VolunteerForm from "./pages/volunteerpg/volunteerform";
+import NGOSignUp from './pages/signinpage/ngosignup'; // ✅ Corrected
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/volunteer" element={<VolunteerPage />} /> 
         <Route path="/volunteer1" element={<VolunteerForm />} /> 
-
         <Route path="/login" element={<LoginPage />} />
         <Route path="/popupsignin" element={<SigninPage />} />
+        <Route path="/Signinpage" element={<SigninPage />} />
+        <Route path="/ngosignup" element={<NGOSignUp />} /> {/* ✅ Corrected */}
       </Routes>
     </Router>
   );
