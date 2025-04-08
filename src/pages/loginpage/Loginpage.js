326195f2125e27627fbf67c324a6aec7fcbaec6f
gargipratapwar@gmail.com <gargipratapwar@gmail.com>
2025-04-08 19:48:26 +0530

    Project done

--- src/pages/loginpage/Loginpage.js
+++ src/pages/loginpage/Loginpage.js
@@ -1,53 +1,39 @@
 import React from 'react';
- import { FaUser, FaLock } from 'react-icons/fa';
- import './loginpage.css';
- 
- const LoginPage = () => {
-   return (
-     <div className="login-wrapper">
-   <form className="login-card">
-       <form className="login-card">
-         <h1 className="login-title">Login</h1>
- 
-     <h1 className="login-title">Login</h1>
-     
-     <div className="input-container">
-       <FaUser className="icon" />
-       <input type="text" placeholder="Username" required />
-     </div>
-         {}
-         <div className="flex-input-group">
-           <div className="input-container">
-             <FaUser className="icon" />
-             <input type="text" placeholder="Username" required />
-           </div>
-           <div className="input-container">
-             <FaLock className="icon" />
-             <input type="password" placeholder="Password" required />
-           </div>
-         </div>
- 
-     <div className="input-container">
-       <FaLock className="icon" />
-       <input type="password" placeholder="Password" required />
-     </div>
-                                                                
-     <button className="login-button" type="submit">Login</button>
-         <button className="login-button" type="submit">Login</button>
- 
-     <div className="login-subtext">
-       <p>Don’t have an account?</p>
-       <p className="create-account">Create Account</p>
-         <div className="login-subtext">
-           <p>Don’t have an account?</p>
-           <p className="create-account">Create Account</p>
-         </div>
-         </div>
-       </form>
-       </form>
-     </div>
- 
-   );
- };
- 
- export default LoginPage;
+import { FaUser, FaLock } from 'react-icons/fa';
+import './loginpage.css';
+
+const LoginPage = () => {
+  return (
+    <div className="signin-container">
+      <form className="signin-card">
+        <h1 className="signin-title">Login</h1>
+
+        <div className="input-container">
+          <FaUser className="icon" />
+          <input type="text" placeholder="Username" required />
+        </div>
+
+        <div className="row">
+          <div className="input-container">
+            <FaUser className="icon" />
+            <input type="text" placeholder="Set Password" required />
+          </div>
+          <div className="input-container">
+            <FaLock className="icon" />
+            <input type="text" placeholder="Confirm Password" required />
+          </div>
+        </div>
+
+        
+        <button className="signup-button" type="submit">Login</button>
+
+        <div className="signin-subtext">
+          <p>Don’t have an account?</p>
+          <p className="create-account">Create Account</p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
