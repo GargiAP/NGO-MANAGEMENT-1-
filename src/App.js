851aff323274b4430860faf040a8b52fa3dc9ba4
Anchalmunot <anchalmunot223@gmail.com>
2025-05-20 09:24:20 +0530

    volunteer form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/Homepage";
+import VolunteerPage from "./pages/volunteerpg/Volunteerpg"; 
 import LoginPage from "./pages/loginpage/Loginpage";
 import SigninPage from "./pages/signinpage/Signinpage";
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/volunteer" element={<VolunteerPage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signin" element={<SigninPage />} />
       </Routes>
